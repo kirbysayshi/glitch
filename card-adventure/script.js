8a5c903901f45410a55101c9b46fab6aadd7cedb
Glitch (calm-cork) <none>
2018-03-17 18:15:56 +0000

    🗺💘 Checkpoint ./script.js:691428/1209 ./index.html:691428/129

--- card-adventure/script.js
+++ card-adventure/script.js
@@ -7,7 +7,14 @@ const RoomList = [
     onEnter: function (game) {
       console.log('onenter foyer!', game);
     }
-  }  
+  },
+  {
+    id: 'study',
+    name: 'Study',
+    onEnter: function (game) {
+      console.log('onenter study!', game);
+    }
+  }
 ]
 
 const ItemDeck = [
@@ -22,3 +29,21 @@ const ItemDeck = [
     }
   }
 ]
+
+const scrollArea = document.createElement('div');
+document.body.appendChild(scrollArea);
+
+const input = document.createElement('input');
+input.type = 'text';
+input.onkeydown = e => {
+  if (e.key === 'Enter') {
+    console.log('ENTER!');  
+  }
+}
+
+const statusArea = document.createElement('div');
+statusArea.style.width = '100vw';
+statusArea.style.width = '100vw';
+statusArea.appendChild(input);
+
+document.body.appendChild(statusArea);
